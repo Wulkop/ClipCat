b5197187523916b51a0ac77a4c9d549a8b869ec6
Jan <jan.Wulop@web.de>
2020-11-12 02:26:22 +0100

    Added overwolf simple IO plugin dependency
    -Use IO plugin for file check and move operations
    -Removed JSMQ dependency

--- button_event_handler.js
+++ button_event_handler.js
@@ -11,6 +11,37 @@ function hideButtons()
     $("#Buttons").children().hide();
 }
 
+function moveLatestClip(videoPath, category)
+{
+  plugin.get().getLatestFileInDirectory(videoPath + "/*", function(status, filename, lastWriteTime)
+  {
+    if (status)
+    {
+      currentTimeSecs = Math.floor(Date.now() / 1000);
+      if (lastWriteTime > currentTimeSecs)
+      {
+        console.warn("Check your local time settings. They might be wrong on your computer");
+        console.warn("Current TS: " + currentTimeSecs + " FileModified: " + lastWriteTime);
+        console.warn("Clip will not be moved");
+        return;
+      }
+      if (currentTimeSecs - lastWriteTime > 60)
+      {
+        console.warn("Last clip in folder is older than 1 minute. Are make sure your recording program actually recorded something.");
+        return;
+      }
+      plugin.get().moveFile(videoPath, filename, category, function(status, message)
+      {
+        console.log(message);
+      });
+    }
+    else
+    {
+      console.log("No file found");
+    }
+  });
+}
+
 function onButtonClick(clicked_id)
 {
     console.log(clicked_id)
@@ -27,7 +58,10 @@ function onButtonClick(clicked_id)
     {
         return;
     }
-    onClipClassified(clicked_id);
+    setTimeout(function()
+    {
+        onClipClassified(clicked_id);
+    }, 15000);
     hideButtons();
 }
 function onNameButtonClick(clicked_id)
@@ -36,5 +70,9 @@ function onNameButtonClick(clicked_id)
     $('#NameSelect').css('visibility','hidden');
     $("#NameSelect").children().hide();
 
-    onClipClassified(mode + "_" + clicked_id);
+    setTimeout(function()
+    {
+        onClipClassified(mode + "_" + clicked_id);
+    }, 15000);
+    hideButtons();
 }
--- main.js
+++ main.js
@@ -12,7 +12,13 @@ var g_interestedInFeatures = [
   'death',
   'match_info'
 ];
-var dealer = new JSMQ.Dealer();
+//var dealer = new JSMQ.Dealer();
+var plugin = new OverwolfPlugin("simple-io-plugin", true);
+var config_path = "D:/Development/events-sample-apps-master/rainbow-six-sample-app/config.json";
+var config = 
+{
+  path: "D:/ShadowPlayTest/Tom Clancy's Rainbow Six  Siege"
+}
 
 function registerEvents() {
   // general events errors
@@ -99,51 +105,103 @@ function setFeatures() {
     console.log(JSON.stringify(info));
   });
 }
-
-
 function send(msgString) {
   var message = new JSMQ.Message();
   message.addString(msgString);
-  dealer.send(message);
+  //dealer.send(message);
 }
-
-
-// Start here
-overwolf.games.onGameInfoUpdated.addListener(function (res) {
-  if (gameLaunched(res)) {
-    registerEvents();
-    setTimeout(setFeatures, 1000);
-  }
-  console.log("onGameInfoUpdated: " + JSON.stringify(res));
-});
-overwolf.settings.hotkeys.onPressed.addListener(function(info)
-{
-  console.log("On Pressed");
-  showButtons()
-});
-
-overwolf.games.getRunningGameInfo(function (res) {
-  if (gameRunning(res)) {
-
-    console.log("Create JSMQ connection")
-    dealer.connect("ws://127.0.0.1:86");
-
-
-    registerEvents();
-    setTimeout(setFeatures, 1000);
-  }
-  send("getRunningGameInfo: " + JSON.stringify(res));
-  console.log("getRunningGameInfo: " + JSON.stringify(res));
-});
-
-overwolf.settings.getOverwolfVideosFolder(function(res)
-{
-  console.log(res);
-});
 function onClipClassified(classification)
 {
-  var videoPath = "D:/ShadowPlayTest/Tom Clancy's Rainbow Six  Siege"
-  var content = "{\"clip\":{\"class\": \"" + classification + "\", \"path\":\"" + videoPath + "\" }}\""
-  console.log(content)
-  send(content)
+  if(config == null)
+  {
+    return;
+  }
+
+  var videoPath = config.path; // no extension filtering
+  moveLatestClip(videoPath, classification)
 }
+function onConfigWriteCompleted(status)
+{
+  console.log(status);
+}
+function onConfigReadCompleted(status)
+{
+  console.log(status);
+  if(status.success)
+  {
+    config = JSON.parse(status.content)
+  }
+  else
+  {
+    console.warn("Cannot parse config content");
+    config = null;
+  }
+}
+function readConfig()
+{
+  overwolf.io.fileExists(config_path, function(status)
+  {
+    if(status.status == "error")
+    {
+      console.warn(status.found);
+    }
+    if(!status.found)
+    {
+      console.log("Config file does not exist");
+      config.path = window.prompt("Please enter the path to your video recordings", "D:/ShadowPlayTest/Tom Clancy's Rainbow Six  Siege");
+     
+      configStr = JSON.stringify(config);
+      overwolf.io.writeFileContents(config_path, configStr, "UTF8", false, onConfigWriteCompleted);
+    }
+    else if (status.status == "success" && status.found)
+    {
+      overwolf.io.readFileContents(config_path, "UTF8", onConfigReadCompleted);
+    }
+  });
+}
+
+function main(status)
+{
+
+  if (status == false)
+  {
+    console.log("Plugin couldn't be loaded??");
+    return;
+  }
+
+  readConfig();
+
+  // Start here
+  overwolf.games.onGameInfoUpdated.addListener(function (res) {
+    if (gameLaunched(res)) {
+      registerEvents();
+      setTimeout(setFeatures, 1000);
+    }
+    console.log("onGameInfoUpdated: " + JSON.stringify(res));
+  });
+  overwolf.settings.hotkeys.onPressed.addListener(function(info)
+  {
+    console.log("On Pressed");
+    showButtons()
+  });
+  overwolf.games.getRunningGameInfo(function (res) {
+    if (gameRunning(res)) {
+
+      console.log("Create JSMQ connection")
+      //dealer.connect("ws://127.0.0.1:86");
+
+
+      registerEvents();
+      setTimeout(setFeatures, 1000);
+    }
+    send("getRunningGameInfo: " + JSON.stringify(res));
+    console.log("getRunningGameInfo: " + JSON.stringify(res));
+  });
+}
+plugin.initialize(main)
+
+
+
+
+
+
